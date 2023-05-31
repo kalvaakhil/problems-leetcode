fb876597aff58d7d66b83bf485ccbfc9d240e104
Kalva Akhil Reddy <kalvaakhilreddy080@gmail.com>
2023-05-31 11:40:52 +0530

    samll changes

---
+++ DSA IN JAVA/quque.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Queue {
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+        if(q.isEmpty()){
+            System.out.println("Empty");
+        }
+    }
+}
